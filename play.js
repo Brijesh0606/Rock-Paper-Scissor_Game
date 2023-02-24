205fbc9ac4f65146bcef70f8442ebc8a66748797
Brijesh0606 <brijeshbhut.it20@scet.ac.in>
2023-02-24 18:56:34 +0530

    Version 2.1

--- play.js
+++ play.js
@@ -1,4 +1,4 @@
- //1,2,3 random number
+//1,2,3 random number
 //1=rock
 //2=paper
 //3=scissor
@@ -11,20 +11,15 @@ function select(id) {
     const resultcomp = document.getElementById('resultcomp');
     const resultyou = document.getElementById('resultyou');
 
-    if (player == x)
-    {
-        result.innerHTML="Game is Draw!!";
-        result.style.backgroundColor = 'Grey';
-        resultcomp.textContent = "Play Again!!!!"
-        resultyou.textContent = "To Play Click on Image!"
+    if (player == x) {
+        game_draw();
     }
     if (player == 1) {
         if (x == 2) {
-            result.innerHTML = "Computer Win the Game";
-            result.style.backgroundColor = 'Red';
+            computer_win();
             resultcomp.textContent = 'Computer : Paper';
             resultyou.textContent = 'You : Rock';
-            
+
         }
         else if (x == 3) {
             result.innerHTML = "You Win the Game";
@@ -35,31 +30,43 @@ function select(id) {
     }
     else if (player == 2) {
         if (x == 1) {
-            result.innerHTML = "You Win the Game";
-            result.style.backgroundColor = 'Green';
+            you_win();
             resultcomp.textContent = 'Computer : Rock';
             resultyou.textContent = 'You : Paper';
         }
         else if (x == 3) {
-            result.innerHTML = "Computer Win the Game";
-            result.style.backgroundColor = 'Red';
+            computer_win();
             resultcomp.textContent = 'Computer : Scissor';
             resultyou.textContent = 'You : Paper';
         }
     }
     else if (player == 3) {
         if (x == 1) {
-            result.innerHTML = "Computer Win the Game";
-            result.style.backgroundColor = 'Red';
+            computer_win();
             resultcomp.textContent = 'Computer : Rock';
             resultyou.textContent = 'You : Scissor';
         }
         else if (x == 2) {
-            result.innerHTML = "You Win the Game";
-            result.style.backgroundColor = 'Green';
+            you_win();
             resultcomp.textContent = 'Computer : Paper';
             resultyou.textContent = 'You : Scissor';
         }
     }
-    
+}
+
+function game_draw() {
+    result.innerHTML = "Game is Draw!!";
+    result.style.backgroundColor = 'Grey';
+    resultcomp.textContent = "Play Again!!!!"
+    resultyou.textContent = "To Play Click on Image!"
+}
+
+function computer_win() {
+    result.innerHTML = "Computer Win the Game";
+    result.style.backgroundColor = 'Red';
+}
+
+function you_win() {
+    result.innerHTML = "You Win the Game";
+    result.style.backgroundColor = 'Green';
 }
